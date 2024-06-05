f8b028ca2d30550b3919ea0430ad0ff53878f7a9
Sharif101 <sharif009009@gmail.com>
2024-06-05 16:12:48 +0600

    1042 trying

---
+++ BeecrowdProblem/1042.js
@@ -0,0 +1,20 @@
+var input = require("fs").readFileSync("./BeecrowdProblem/input.txt", "utf8");
+var lines = input.split("\n");
+
+let values = lines[0].split(" ");
+
+let x = parseInt(values[0]);
+let y = parseInt(values[1]);
+let z = parseInt(values[2]);
+
+function assending(a, b, c) {
+  let arry = [a, b, c];
+  let assing = arry.sort((a, b) => a - b);
+
+  let finalAssing = assing.join("\n");
+  let input = `\n${a}\n${b}\n${c}`;
+
+  return `${finalAssing} \n${input}`;
+}
+
+console.log(assending(x, y, z));
